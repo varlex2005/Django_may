b0eb0cc088c18f470f65a5d449e83e4f8e07dc0d
vyachaslaushkadarevich <varlex2005@gmail.com>
2023-05-26 21:17:42 +0300

    2st release django 2

--- horoscope/admin.py
+++ horoscope/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Genre
 
 # Register your models here.
 
 admin.site.register(Book)
+admin.site.register(Genre)
--- horoscope/models.py
+++ horoscope/models.py
@@ -6,11 +6,16 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
+    raiting = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return f'Книга: {self.id}  Название:  {self.title} Автор: {self.author}'
 #         строковое представление объекта (записи в БД)
-oo
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=50)
+
 
 # эту таблицу надо теперь записать в БД python3 manage.py makemigrations -
 # выполнить в терминале и потом python3 manage.py migrate. Не забыть,
